aa027cee44e9eb3a6fb221a82117802f8b9f9c70
Kat Iaro <ekaterinayaroshovets@gmail.com>
2019-01-26 13:09:43 +0100

    dodaje Usera

--- src/main/java/sda/UserService.java
+++ src/main/java/sda/UserService.java
@@ -1,4 +1,16 @@
 package sda;
 
 public class UserService {
+
+    public User[] users = new User [5];
+    private int count = 0;
+
+    public  void addUser(User user){
+        users[count++]=user;
+    }
+    public void printAll(){
+        for(int i = 0; i <users.length;i++){
+            System.out.println(users[i]);
+        }
+    }
 }
