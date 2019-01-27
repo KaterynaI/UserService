01fdc58034ca77ca25249a877337d828b120d0a1
Kat Iaro <ekaterinayaroshovets@gmail.com>
2019-01-27 14:31:36 +0100

    Set

---
+++ src/main/java/sda/Country.java
@@ -0,0 +1,20 @@
+package sda;
+
+public class Country {
+
+  private  String nazwaKraju;
+   private String stolica;
+   private int liczbaLudnosc;
+
+    public Country(String nazwaKraju, String stolica, int liczbaLudnosc ){
+        this.nazwaKraju = nazwaKraju;
+        this.stolica = stolica;
+        this.liczbaLudnosc = liczbaLudnosc;
+
+    }
+
+
+
+
+
+}
